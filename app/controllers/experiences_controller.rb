80600d87f258e9dc3615604567f6e9e74a50e7e4
Hennau Pierre <pphennau@gmail.com>
2013-10-19 02:16:54 +0900

    adding 2 images and adding some checks in GA

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,10 +1,19 @@
 class ExperiencesController < ApplicationController
 
+	GA_MAX_GEN = 2000
+	GA_MAX_POP = 300
+
 	def genetic_typer_run
 		@num_gen = params[:num_gen].to_i
+		@num_gen = GS_MAX_GEN if @num_gen > GS_MAX_GEN
+		@num_gen = 1 if @num_gen < 1
 		@num_pop = params[:num_pop].to_i
+		@num_pop = GA_MAX_POP if @num_pop > GA_MAX_POP
+		@num_pop = 1 if @num_pop < 1
 		@sentence = params[:sentence]
 		@mutation_rate = params[:mutation_rate].to_f
+		@mutation_rate = 100 if @mutation_rate > 100
+		@mutation_rate = 0 if @mutation_rate < 1
 
 		ga = GeneticTyper::GeneticAlgorithm.new(@sentence, @num_pop, @num_gen, @mutation_rate)
 		@first_generation = ga.population.map {|dna| dna.genes}
